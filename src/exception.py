36e0cd44ae704f4bf7b64f6b48f6dcf7ce8d74e8
Prashant SRiMali <er.prashantkmrmali@gmail.com>
2024-03-05 00:27:56 +0530

    Components added

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+import logger
+
+def error_message_details(error, error_detail:sys):
+    _, _, execution_tb = error_detail.exc_info()
+    file_name = execution_tb.tb_frame.f_code.co_filename
+    err_message = "Error occured in File_name [{0}], at line number [{1}] and error message: [{2}]".format(
+        file_name, execution_tb.tb_lineno, str(error)
+    )
+
+    return err_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail=error_detail)
+
+
+    def __str__(self) -> str:
+        return self.error_message
